a60a03fc39954629f563b9e432571ea1565dc2bd
figankilic <53525621+figankilic@users.noreply.github.com>
2019-10-21 18:28:48 +0300

    c programlama matematik işlemi

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,x=9,y=10,z=11;
+    float t=2*(a*b);
+    float m=(e*f)-g;
+    float n=c*d;
+    float s=m/h;
+    float w=c*d*s;
+    float v=(y+z)*x;
+    float o=1/t;
+    float k=(o-w)/v;
+    printf("%f",k);
+    return 0;
+}
